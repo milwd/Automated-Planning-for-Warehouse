ea013c9b9c42c3bc4fa0190c64b46a736b140419
ShadyEgypt <shadyrafat60@gmail.com>
2025-05-19 16:37:01 +0200

    add a bash script to use the solver locally

---
+++ run_enhsp19.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Check if an argument is provided
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <problem-file>"
+    exit 1
+fi
+
+PROBLEM_FILE="$1"
+DOMAIN_FILE="maindomain.pddl"
+ENHSP_DIR="enhsp-19/nbdist"
+
+# Run the solver
+java -jar "$ENHSP_DIR/enhsp-19.jar" enhsp.Main -o "$DOMAIN_FILE" -f "$PROBLEM_FILE"
